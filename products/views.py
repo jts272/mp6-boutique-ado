51cdca72af6d15ad655f68cf848e2b650e92bb5c
Joseph Smith <jts272@outlook.com>
2023-06-09 01:22:30 +0100

    feat: add category filtering

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
@@ -13,9 +13,12 @@ def all_products(request):
     products = Product.objects.all()
     # Give no search term value to context by default (i.e. empty search)
     query = None
+    # Give `None` value to category for context dict if not used
+    categories = None
 
     # Access URL parameters from form get
     if request.GET:
+        print(request.GET)
         # Reference `name` of text input `q`
         if "q" in request.GET:
             # Create var for search query
@@ -37,8 +40,30 @@ def all_products(request):
             # Perform the actual filtering with the Q objects
             products = products.filter(queries)
 
+        # If `category` is in the URL from the GET request from nav links
+        if "category" in request.GET:
+            """`category` has been created as key for the QueryDict
+            object when `?category=<categories>` was given as the href
+            in `main_nav.html`.
+
+            https://developer.mozilla.org/en-US/docs/Web/API/URL/search
+            """
+            # Get the csv list of categories from the href and split commas
+            # dict lookup with square brackets, not a method call
+            categories = request.GET["category"].split(",")
+            # Use these categories to filter the products for the context
+            # https://docs.djangoproject.com/en/3.2/topics/db/queries/#lookups-that-span-relationships
+            # https://docs.djangoproject.com/en/3.2/ref/models/querysets/#in
+            products = products.filter(category__name__in=categories)
+            # Also get the selected categories to show in the template
+            categories = Category.objects.filter(name__in=categories)
+
     # The `query` must be provided to the context
-    context = {"products": products, "search_term": query}
+    context = {
+        "products": products,
+        "search_term": query,
+        "current_categories": categories,
+    }
 
     return render(request, "products/products.html", context)
 
