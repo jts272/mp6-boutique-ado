096b69d8cb5ef39638bf5e98c447535ae911ac84
Joseph Smith <jts272@outlook.com>
2023-06-13 23:06:19 +0100

    feat: update context processor with bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,9 @@
 from decimal import Decimal
+
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
 
 
 def bag_contents(request):
@@ -9,6 +13,27 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    # Get bag or init. Origin: `add_to_bag` in views.py
+    bag = request.session.get("bag", {})
+
+    # Iterate through k:v items in the shopping bag to display on site
+    # This is the session bag
+    for item_id, quantity in bag.items():
+        # Get the product first for calculations and information
+        product = get_object_or_404(Product, pk=item_id)
+        # Total is the quantity of each product's price
+        total += product.price * quantity
+        # Increment the product count by the quantity
+        product_count += quantity
+        # Add dict to list of bag items for access in templates
+        # This is like a context dict for the contents of the bag
+        bag_items.append(
+            {
+                "item_id": item_id,
+                "quantity": quantity,
+                "product": product,
+            }
+        )
 
     # Incentivize customers to meet free shipping threshold
     if total < settings.FREE_DELIVERY_THRESHOLD:
