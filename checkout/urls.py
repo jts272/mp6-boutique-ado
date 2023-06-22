8b1e5c98b1c471d2636550d81ed2e44ed39d5d98
Joseph Smith <jts272@outlook.com>
2023-06-22 12:55:07 +0100

    feat: add checkout view and template

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="checkout")]
+urlpatterns = [path("", views.checkout, name="checkout")]
--- mp6/settings.py
+++ mp6/settings.py
@@ -82,6 +82,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # Give access to `MEDIA_URL` template tag for instances of no image
+                "django.template.context_processors.media",
                 # Custom context processor for shopping bag
                 "bag.contexts.bag_contents",
             ],
