61fe1954bbec0a01cc36c882424aea0e28cef0da
Joseph Smith <jts272@outlook.com>
2023-07-05 19:05:38 +0100

    feat: add profile form, views and template
    The JS to colour the country placeholder from the lesson material does not work.

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render
 
+from .forms import UserProfileForm
 from .models import UserProfile
 
 
@@ -7,7 +9,25 @@ def profile(request):
     """Display the user's profile."""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    # Populate form with current user's profile information
+    form = UserProfileForm(instance=profile)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+
+    # Use related name on the order model to get order history
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
-    context = {"profile": profile}
+    context = {
+        # "profile": profile,
+        "form": form,
+        "orders": orders,
+        # For profile update success message toasts
+        "on_profile_page": True,
+    }
 
     return render(request, template, context)
