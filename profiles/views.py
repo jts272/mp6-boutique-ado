fd54e204a6c8626417e85ae6942fba963f7b63f9
Joseph Smith <jts272@outlook.com>
2023-07-06 12:44:37 +0100

    feat: add order history and update checkout view
    The prepopulated form containing user profile information does not work.

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, render
 
+# For order history
+from checkout.models import Order
+
 from .forms import UserProfileForm
 from .models import UserProfile
 
@@ -28,6 +31,28 @@ def profile(request):
         "orders": orders,
         # For profile update success message toasts
         "on_profile_page": True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(
+        request,
+        (
+            f"This is a past confirmation for order number {order_number}. "
+            "A confirmation email was sent on the order date."
+        ),
+    )
+
+    # Re-use order success template
+    template = "checkout/checkout_success.html"
+    context = {
+        "order": order,
+        # If user got to the checkout_success template from the order_history view
+        "from_profile": True,
     }
 
     return render(request, template, context)
